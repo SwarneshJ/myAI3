2ab4197cabeb9f5c995e300f7a7791fa62a95a8e
Swarnesh <swarnesh.jha2011@gmail.com>
2026-01-12 19:31:59 -0500

    Improve styling for user message component
    Updated the styling of the user message component.

--- components/messages/user-message.tsx
+++ components/messages/user-message.tsx
@@ -4,7 +4,8 @@ import { Response } from "@/components/ai-elements/response";
 export function UserMessage({ message }: { message: UIMessage }) {
     return (
         <div className="whitespace-pre-wrap w-full flex justify-end">
-            <div className="max-w-lg w-fit px-4 py-3 rounded-[20px] bg-neutral-100">
+            {/* UPDATED STYLING HERE: */}
+            <div className="max-w-lg w-fit px-5 py-3 rounded-[20px] rounded-tr-sm bg-[#007c7c] text-white shadow-sm">
                 <div className="text-sm">
                     {message.parts.map((part, i) => {
                         switch (part.type) {
